ee41fad8c6cc3f0723b9642e10ab91ce7dab1be8
Dmytro Pochapskyi <pchpsky@outlook.com>
2023-07-20 19:24:00 +0300

    refactoring

--- lib/fuel_calculator.rb
+++ lib/fuel_calculator.rb
@@ -1,35 +1,37 @@
 class FuelCalculator
-  def calculate_total_fuel(mass, flight_route)
+  def fuel_for_route(initial_mass, flight_route)
     flight_route.reverse.reduce(0) do |total_fuel, (action, gravity)|
-      total_fuel + calculate_additional_fuel(mass + total_fuel, action, gravity)
+      total_mass = initial_mass + total_fuel
+      total_fuel + calculate_fuel(total_mass, action, gravity)
     end
   end
 
-  def calculate_additional_fuel(fuel, action, gravity)
-    additional_fuel = 0
-    loop do
-      fuel = calculate_fuel(fuel, action, gravity)
-      break if fuel <= 0
-
-      additional_fuel += fuel
-    end
-
-    additional_fuel
-  end
+  private
 
   def calculate_fuel(mass, action, gravity)
+    total_fuel(mass, fuel_formula(action, gravity))
+  end
+
+  def total_fuel(mass, fuel_formula)
+    Enumerator
+      .produce(fuel_formula.call(mass), &fuel_formula)
+      .take_while(&:positive?)
+      .sum
+  end
+
+  def fuel_formula(action, gravity)
     if action == :launch
-      calculate_fuel_for_launch(mass, gravity)
+      ->(mass) { fuel_for_launch(mass, gravity) }
     else
-      calculate_fuel_for_landing(mass, gravity)
+      ->(mass) { fuel_for_landing(mass, gravity) }
     end
   end
 
-  def calculate_fuel_for_launch(mass, gravity)
+  def fuel_for_launch(mass, gravity)
     (mass * gravity * 0.042 - 33).floor
   end
 
-  def calculate_fuel_for_landing(mass, gravity)
+  def fuel_for_landing(mass, gravity)
     (mass * gravity * 0.033 - 42).floor
   end
 end
--- spec/fuel_calculator_spec.rb
+++ spec/fuel_calculator_spec.rb
@@ -3,9 +3,9 @@ require 'fuel_calculator'
 RSpec.describe FuelCalculator do
   subject { described_class.new }
 
-  describe '#calculate_total_fuel' do
+  describe '#fuel_for_route' do
     context 'for landing Apollo 11 on Earth' do
-      let(:mass) { 28801 }
+      let(:mass) { 28_801 }
       let(:flight_route) do
         [
           [:land, 9.807]
@@ -13,12 +13,12 @@ RSpec.describe FuelCalculator do
       end
 
       it 'calculates the total fuel required' do
-        expect(subject.calculate_total_fuel(mass, flight_route)).to eq(13447)
+        expect(subject.fuel_for_route(mass, flight_route)).to eq(13_447)
       end
     end
 
     context 'for Apollo 11 mission' do
-      let(:mass) { 28801 }
+      let(:mass) { 28_801 }
       let(:flight_route) do
         [
           [:launch, 9.807],
@@ -29,12 +29,12 @@ RSpec.describe FuelCalculator do
       end
 
       it 'calculates the total fuel required' do
-        expect(subject.calculate_total_fuel(mass, flight_route)).to eq(51898)
+        expect(subject.fuel_for_route(mass, flight_route)).to eq(51_898)
       end
     end
 
     context 'for mission on Mars' do
-      let(:mass) { 14606 }
+      let(:mass) { 14_606 }
       let(:flight_route) do
         [
           [:launch, 9.807],
@@ -45,12 +45,12 @@ RSpec.describe FuelCalculator do
       end
 
       it 'calculates the total fuel required' do
-        expect(subject.calculate_total_fuel(mass, flight_route)).to eq(33388)
+        expect(subject.fuel_for_route(mass, flight_route)).to eq(33_388)
       end
     end
 
     context 'for passanger ship' do
-      let(:mass) { 75432 }
+      let(:mass) { 75_432 }
       let(:flight_route) do
         [
           [:launch, 9.807],
@@ -63,7 +63,7 @@ RSpec.describe FuelCalculator do
       end
 
       it 'calculates the total fuel required' do
-        expect(subject.calculate_total_fuel(mass, flight_route)).to eq(212161)
+        expect(subject.fuel_for_route(mass, flight_route)).to eq(212_161)
       end
     end
   end
